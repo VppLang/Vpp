f23c7d0656e1008973fd0ceaa9f48831719a4e25
Wyatt Degenhart <43429182+WittySmirk@users.noreply.github.com>
2020-05-23 13:20:26 -0700

    Create Token.hpp

---
+++ include/Token.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "TokenType.hpp"
+
+namespace token
+{
+	
+	class Token
+	{
+	public:
+		Token();
+		Token(TokenType p_type, const std::string& p_lexeme, const std::string& p_literal, int p_line);
+
+		TokenType getType() const { return type; }
+
+		const std::string& getLexeme() const { return lexeme; }
+
+		const std::string& getLiteral() const { return literal; }
+
+		int getLine() const { return line; }
+
+	private:
+		TokenType type;
+		std::string lexeme;
+		std::string literal;
+
+		int line;
+	};
+
+}
