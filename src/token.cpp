141c1d2e5db844b0c63f1670edfc04c982ef6bee
codergopher <57048930+codergopher@users.noreply.github.com>
2020-05-23 12:28:13 -0600

    Add files via upload

---
+++ src/token.cpp
@@ -0,0 +1,24 @@
+#include "Token.hpp"
+
+#include <iostream>
+#include <sstream>
+
+namespace token
+{
+
+	Token::Token()
+	:type(token::TokenType::INVALID),
+	lexeme("empty lexeme"),
+	literal("empty literal"),
+	line(-1)
+	{
+	}
+
+	Token::Token(TokenType p_type, const std::string& p_lexeme, const std::string& p_literal, int p_line)
+	:type(p_type),
+	lexeme(p_lexeme),
+	literal(p_literal),
+	line(p_line)
+	{
+	}
+}
